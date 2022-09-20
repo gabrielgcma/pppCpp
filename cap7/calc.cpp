86f78350fe51c942497737fd17a963d6225f69e8
Gabriel Amado <amadocmgabriel@gmail.com>
2022-09-20 20:39:27 -0300

    7.6.4

--- cap7/calc.cpp
+++ cap7/calc.cpp
@@ -1,3 +1,52 @@
+/*
+
+    Simple Calculator 
+
+    Revision history: 
+            Revised by Bjarne Stroustrup November 2013
+            Revised by Bjarne Stroustrup May 2007
+            Revised by Bjarne Stroustrup August 2006
+            Revised by Bjarne Stroustrup August 2004
+            Originally written by Bjarne Stroustrup
+                    (bs@cs.tamu.edu) Spring 2004.
+
+    This program implements a basic expression calculator.
+    Input from cin; output to cout.
+    The gramamar for input is:
+
+    Statement:
+        Expression 
+        Print
+        Quit
+
+    Print:
+        ;
+    
+    Quit:
+        q;
+
+    Expression:
+        Term
+        Expression + Term
+        Expression - Term
+    Term:
+        Primary
+        Term * Primary
+        Term / Primary
+        Term % Primary
+    Primary:
+        Number
+        ( Expression )
+        - Primary
+        + Primary
+    Number:
+        floating-point literal
+
+    
+    Input comes from cin trough the Token_stream called ts.
+
+*/
+
 #include "std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
